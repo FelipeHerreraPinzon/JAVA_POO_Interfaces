92ddf8ff7b741a55c24464c84710adfd5d006591
Juan Felipe Herrera Pinzón <77707590+FelipeHerreraPinzon@users.noreply.github.com>
2024-06-27 04:46:12 -0500

    creando clase Curriculo

---
+++ src/main/java/org/factordev/poointerfaces/imprenta/modelo/Curriculo.java
@@ -0,0 +1,37 @@
+package org.factordev.poointerfaces.imprenta.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Curriculo extends Hoja{
+
+    private String persona;
+    private String carrera;
+    private List<String> experiencias;
+
+    public Curriculo(String contenido, String persona, String carrera, List<String> experiencias) {
+        super(contenido);
+        this.persona = persona;
+        this.carrera = carrera;
+        this.experiencias = new ArrayList<>();
+    }
+
+    public Curriculo addExperiencia(String exper){
+        experiencias.add(exper);
+        return this;
+    }
+
+    @Override
+    public String Imprimir() {
+        StringBuilder sb = new StringBuilder("Nombre: ");
+        sb.append(persona).append("\n")
+                .append("Resumen: ").append(this.contenido)
+                .append("\n").append("Profesion: ")
+                .append(this.carrera).append("\n")
+                .append("Experiencias: \n");
+        for(String exp: this.experiencias){
+            sb.append("").append(exp).append("\n");
+        }
+        return sb.toString();
+    }
+}
